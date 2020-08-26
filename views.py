49d513811149304adc509c43e62e8d4ccff9e8bf
Shizo-Shizo <mc2017artemklochkcov435@yandex.ru>
2020-08-26 09:45:21 +0300

    Second commit

--- views.py
+++ views.py
@@ -33,7 +33,7 @@ def bot():
                 # Только в личных сообщениях
                 if peer_id == from_id:
                     if text.lower() == 'привет' or payload['button'] == '2':
-                        session.send_message(peer_id, text="ну хай.", keyboard={})
+                        session.send_message(peer_id, text="ну хай.", keyboard={'buttons': []})
                 # Только в беседах
                 elif peer_id != from_id:
                     pass
